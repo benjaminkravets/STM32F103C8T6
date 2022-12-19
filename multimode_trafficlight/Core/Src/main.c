a995d8b22f81951b0c6e5d08c6bb2e3eff2b3d0f
benjaminkravets <bakravets@gmail.com>
2022-12-19 10:46:46 -0500

    test

--- multimode_trafficlight/Core/Src/main.c
+++ multimode_trafficlight/Core/Src/main.c
@@ -96,6 +96,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  /* Loop through 3 LEDs and stay on them depending on the PWM_Delay variable.*/
   while (1)
   {
       while(CH1_DC < 65535)
